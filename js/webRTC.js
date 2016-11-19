cc47895f902264869dbeeae3b0282319f4cadd53
Alex de Mulder <alexdemulder@gmail.com>
2016-11-20 00:11:13 +0100

    adding events

--- js/webRTC.js
+++ js/webRTC.js
@@ -1,16 +1,10 @@
 let webrtc = undefined;
 let USER_TYPE = undefined;
+let ROOM_ID = "testing_crownstone";
+
 
 function select(userType) {
   USER_TYPE = userType;
-
-  document.getElementById("startInterface").style.display = "none";
-  if (userType === 'elderly') {
-    document.getElementById("elderlyInterface").style.display = "block";
-  }
-  else {
-    document.getElementById("helpdeskInterface").style.display = "block";
-  }
 
   webrtc = new SimpleWebRTC({
     // the id/element dom element that will hold "our" video
@@ -21,10 +15,44 @@ function select(userType) {
     autoRequestMedia: true
   });
 
+  document.getElementById("startInterface").style.display = "none";
+  if (userType === 'elderly') {
+    document.getElementById("elderlyInterface").style.display = "block";
+  }
+  else {
+    document.getElementById("helpdeskInterface").style.display = "block";
+
+  }
+
+
   // we have to wait until it's ready
   webrtc.on('readyToCall', function () {
     // you can name it anything
     console.log("starting@")
-    webrtc.joinRoom('HELPDESKpoppa123456');
+    webrtc.joinRoom('apoliiut');
+  });
+
+  // local p2p/ice failure
+  webrtc.on('iceFailed', function (peer) {
+    console.log('local fail', peer.sid);
+  });
+
+  // remote p2p/ice failure
+  webrtc.on('connectivityError', function (peer) {
+    console.log('remote fail', peer.sid);
+  });
+
+  // working around weird simplewebrtc behaviour
+  webrtc.on('videoAdded', function (video, peer) {
+    console.log('video added')
+  });
+  // called when a peer is created
+  webrtc.on('createdPeer', function (peer) {
+    console.log('created peer')
+  });
+
+
+  webrtc.connection.on('message', function (message) {
+    console.log('RECEIVED MESSAGE', message);
   });
 }
