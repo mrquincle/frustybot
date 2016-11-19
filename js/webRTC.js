649e8a29e60b255348206aff544a531c071e47e2
Alex de Mulder <alexdemulder@gmail.com>
2016-11-19 21:36:32 +0100

    testing simplewebrtc

--- js/webRTC.js
+++ js/webRTC.js
@@ -1,4 +1,4 @@
-let USER_PEER = undefined;
+let webrtc = undefined;
 let USER_TYPE = undefined;
 let MediaStream = undefined;
 
@@ -18,45 +18,24 @@ function select(userType) {
 
   document.getElementById("startInterface").style.display = "none";
   if (userType === 'elderly') {
-    USER_PEER = new Peer({key: 'yqh0u7lrdrhpvi', secure: true});
     document.getElementById("elderlyInterface").style.display = "block";
-
-    callUser('2eoyrb803qq9qkt9');
   }
   else {
-    USER_PEER = new Peer({key: '2eoyrb803qq9qkt9', secure: true});
     document.getElementById("helpdeskInterface").style.display = "block";
-
-    USER_PEER.on('call', function(callSession) {
-      // Answer the call, providing our mediaStream
-      callSession.answer(MediaStream);
-    });
   }
-}
 
-function callUser(peerId) {
-  console.log("calling!")
-
-  USER_PEER.on('open', function(){
-    console.log('HOPEN')
+  webrtc = new SimpleWebRTC({
+    // the id/element dom element that will hold "our" video
+    localVideoEl: 'localVideo',
+    // the id/element dom element that will hold remote videos
+    remoteVideosEl: 'remotesVideos',
+    // immediately ask for camera access
+    autoRequestMedia: true
   });
 
-  // Receiving a call
-  USER_PEER.on('call', function(call){
-    // Answer the call automatically (instead of prompting user) for demo purposes
-
-  });
-  USER_PEER.on('error', function(err){
-    alert(err.message);
-    // Return to step 2 if error occurs
-
-  });
-
-  let callSession = USER_PEER.call(peerId, MediaStream);
-
-  callSession.on('stream', function(stream) {
-    // `stream` is the MediaStream of the remote peer.
-    // Here you'd add it to an HTML video/canvas element.
-    console.log("call answered")
+  // we have to wait until it's ready
+  webrtc.on('readyToCall', function () {
+    // you can name it anything
+    webrtc.joinRoom('HELPDESK123456');
   });
 }
